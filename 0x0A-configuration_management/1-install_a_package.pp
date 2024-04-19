2ce972799359da5ef3b61199db2eb1621f712712
Sododa <sophieododa@gmail.com>
2024-04-19 14:16:04 +0000

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
-# to install flask
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+# 1-install_a_package.pp
+
+# Ensure the 'python3-pip' package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Use pip3 to install Flask version 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  # Check if Flask 2.1.0 is already installed
+  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
+  require => Package['python3-pip'], # Ensure python3-pip is installed first
+  path    => '/usr/local/bin:/usr/bin', # Add appropriate path for pip3
+}
+
+# Notify the user about the installation
+notify { 'Flask installed successfully':
+  subscribe => Exec['install_flask'],
 }
