ffa0bc6792e97a706e96af49da240e373bf0b51b
Sododa <sophieododa@gmail.com>
2024-04-21 17:53:40 +0000

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,30 +1,12 @@
-# Define SSH client configuration file path
-$ssh_config_file = '/home/ubuntu/.ssh/config'
-
-# Ensure SSH client configuration file exists
-file { $ssh_config_file:
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-# Configure SSH client to use specific private key and disable password authentication
-file_line { 'Set IdentityFile':
-  path    => $ssh_config_file,
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^#?\s*IdentityFile.*',
-  ensure  => present,
-}
-
-file_line { 'Disable PasswordAuthentication':
-  path    => $ssh_config_file,
-  line    => '    PasswordAuthentication no',
-  match   => '^#?\s*PasswordAuthentication.*',
-  ensure  => present,
-}
-
-# Notify the changes applied
-notify { 'SSH client configuration updated':
-  message => 'SSH client configuration updated successfully',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
