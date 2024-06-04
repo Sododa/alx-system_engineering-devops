3c1e7dc42bb49e55d680e2ac6db2d474483a5180
Sododa <sophieododa@gmail.com>
2024-06-04 11:44:39 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
-"""
+Write a function that queries the Reddit API
+and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
 
+Hint: No authentication is necessary for most features of the Reddit API.
+If you’re getting errors related to Too Many Requests,
+ensure you’re setting a custom User-Agent.
+
+Requirements:
+Prototype: def number_of_subscribers(subreddit)
+NOTE: Invalid subreddits may return a redirect to search results.
+Ensure that you are not following redirects.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """
+    If not a valid subreddit, return 0.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = 'User Agent'
+    headers = {
+        'User-Agent': user_agent
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        result = response.json().get('data')
+        return result.get('subscribers')
     else:
         return 0
