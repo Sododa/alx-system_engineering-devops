cfb8b82e93eddebec0fa6e7bfc547777ea5a4cdb
Sododa <sophieododa@gmail.com>
2024-06-04 17:31:36 +0000

    top ten

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns a list containing the
-titles of all hot articles for a given subreddit."""
+"""Module that consumes the Reddit"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], n=0, after=None):
     """ queries the Reddit API and returns a list containing the titles of
-    all hot articles for a given subreddit
-
-    The Reddit API uses pagination for separating pages of responses.
-    If not a valid subreddit, return None.
-
-    Args:
-        subreddit (str): subreddit.
-        hot_list (list, optional): list of titles. Defaults to [].
-
-    Returns:
-        list: list of titles.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'user-agent': 'custom'}
