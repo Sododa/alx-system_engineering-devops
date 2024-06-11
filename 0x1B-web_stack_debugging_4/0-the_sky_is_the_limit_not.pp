78edda99ac32af8bb903274aa23fe4f4684240d9
Sododa <sophieododa@gmail.com>
2024-06-11 15:44:27 +0000

    user limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# we are testing how well our web server setup
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+#user and open a file without any error message.
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
